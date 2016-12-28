ba34e78ade49a999bdc5ecdebc8d16536ed9d90a
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-28 01:28:58 +0100

    fix: script to import data can be ran

--- Granam/CzechVocative/import/import_data.sh
+++ Granam/CzechVocative/import/import_data.sh
@@ -3,16 +3,16 @@ set -x
 set -e
 
 curl https://raw.githubusercontent.com/Mimino666/vokativ/master/vokativ/data/man_suffixes |\
-pickle_to_json.py |\
-json_to_serialized_php.php >\
+./pickle_to_json.py |\
+./json_to_serialized_php.php >\
 ../data/man_suffixes
 
 curl https://raw.githubusercontent.com/Mimino666/vokativ/master/vokativ/data/man_vs_woman_suffixes |\
-pickle_to_json.py |\
-json_to_serialized_php.php >\
+./pickle_to_json.py |\
+./json_to_serialized_php.php >\
 ../data/man_vs_woman_suffixes
 
 curl https://raw.githubusercontent.com/Mimino666/vokativ/master/vokativ/data/woman_first_vs_last_name_suffixes |\
-pickle_to_json.py |\
-json_to_serialized_php.php >\
+./pickle_to_json.py |\
+./json_to_serialized_php.php >\
 ../data/woman_first_vs_last_name_suffixes
